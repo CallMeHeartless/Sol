a7d84a035d2f8c44baa23af381a72cf529f1764d
KOfBlades <k.of.blades@gmail.com>
2018-07-18 13:12:13 +1200

    askjdgakjsdh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 5.0f;
-    public float turnSpeed = 10.0f;
-
-    Camera playerCamera;
-    Rigidbody rb;
-
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+		
 	}
-
-    void FixedUpdate() {
-
-    }
 	
 	// Update is called once per frame
 	void Update () {

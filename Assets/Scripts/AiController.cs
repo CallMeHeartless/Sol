9ca0391c83d5f2581d6df74a22a597a574e4a33f
MarximusPrime <tremere.fist@gmail.com>
2018-07-18 14:50:56 +1200

    Mark's basic AI controller
    BAESIC!

--- Assets/Scripts/AiController.cs
+++ Assets/Scripts/AiController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class AiController : MonoBehaviour {
 
-    private float Speed = 0.1f;
+    private float MaxSpeed = 10.0f;
     private Vector3 currentPos;
     private Vector3 movement;
     private float Radius = 20.0f;
+    private Vector3 targetPos;
+    private int sequence = 1;
+    private float distance;
+    private float CurrentSpeed;
 
 
 	// Use this for initialization
@@ -18,8 +22,60 @@ public class AiController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         currentPos = transform.position;
-        movement.x = 1.0f;
-        movement = movement * Speed;
-        transform.position = movement + currentPos;
+
+        if (sequence == 0)
+        {
+            targetPos = GameObject.Find("Target1").transform.position;
+        }
+
+        if (sequence == 1)
+        {
+            targetPos = GameObject.Find("Target2").transform.position;
+        }
+
+        if (sequence == 2)
+        {
+            targetPos = GameObject.Find("Target3").transform.position;
+        }
+
+        if (sequence == 3)
+        {
+            targetPos = GameObject.Find("Target4").transform.position;
+        }
+
+        //distance = Mathf.Sqrt(Mathf.Pow(2, (targetPos.x - currentPos.x)) + Mathf.Pow(2, (targetPos.z - currentPos.z)));
+        distance = (targetPos - transform.position).magnitude;
+
+        float step = 0;
+
+        if(distance < Radius)
+        {
+            step = MaxSpeed * (distance / Radius) * Time.deltaTime + 0.01f;
+            CurrentSpeed = step;
+        }
+        else
+        {
+            if(CurrentSpeed < MaxSpeed)
+            {
+                CurrentSpeed = CurrentSpeed + 0.1f;
+            }
+
+            
+            step = CurrentSpeed * Time.deltaTime;
+        }
+
+        
+  
+
+        transform.position = Vector3.MoveTowards(currentPos, targetPos, step);
+
+        if (currentPos == targetPos)
+        {
+            sequence++;
+            if(sequence > 3)
+            {
+                sequence = 0;
+            }
+        }
 	}
 }

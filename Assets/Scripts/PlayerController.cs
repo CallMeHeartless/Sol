fd34487490812581d3835ee2c15b2d435d5ad274
KOfBlades <k.of.blades@gmail.com>
2018-07-18 13:01:38 +1200

    Started work on character controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public float turnSpeed = 10.0f;
+
+    Camera playerCamera;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate() {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

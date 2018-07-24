b79f2acd6ff8e242c0b30021bc97c34472bcbeb3
Kerry <k.of.blades@gmail.com>
2018-07-25 09:11:17 +1200

    Sparky now dies when away from Sol

--- Assets/Scripts/AiController.cs
+++ Assets/Scripts/AiController.cs
@@ -161,6 +161,7 @@ public class AiController : MonoBehaviour {
         if(playerDistance > PlayerRadius)
         {
             player.GetComponent<PlayerController>().DrainCharge(Time.deltaTime);
+            //Debug.Log("Draining Sparky");
         }
         else
         {

ea90d1d9090fd957c5e2748966bc3a19eb36748b
Kerry <k.of.blades@gmail.com>
2018-10-03 08:39:12 +1300

    Enemy prefab / Ai controller

---
+++ Assets/Scripts/EnemyAiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAiController : MonoBehaviour {
+
+    [SerializeField]
+    private int m_iLife = 5;
+
+    private bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DamageEnemy(int _iDamage) {
+        m_iLife -= m_iLife;
+        if(m_iLife <= 0) {
+            isAlive = false;
+        }
+    }
+}

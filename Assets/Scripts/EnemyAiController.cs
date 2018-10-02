682e2807bee43cbb650b36fccca41470ffd9d23f
MarximusPrime <39780696+MarximusPrime@users.noreply.github.com>
2018-10-03 08:50:17 +1300

    AI enemy movement

--- Assets/Scripts/EnemyAiController.cs
+++ Assets/Scripts/EnemyAiController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAiController : MonoBehaviour {
 
@@ -9,15 +10,60 @@ public class EnemyAiController : MonoBehaviour {
 
     private bool isAlive = true;
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject player;
+    public NavMeshAgent agent;
+
+    private Ray ray;
+    private Animator anim;
+    private float fDistance;
+
+    public GameObject FindPlayer()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Player");
+        GameObject closest = null;
+        float distance = 100000.0f;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    public void movement()
+    {
+        Vector3 vA = player.transform.position;
+        vA.y = vA.y + 4.0f;
+
+        ray.origin = vA;
+        ray.direction = Vector3.down;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        player = FindPlayer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        movement();
+
+    }
 
     public void DamageEnemy(int _iDamage) {
         m_iLife -= m_iLife;

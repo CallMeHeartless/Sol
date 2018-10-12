489b4ff5d139209f1f2c44b11076fd9f7c501772
Kerry <kerry.pellett@gmail.com>
2018-10-13 12:20:44 +1300

    Added MANY audio files for sound selection.

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    static AudioController instance;
+
+    public enum ESOUND {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void PlaySingleSound(ESOUND sound) {
+        switch (sound) {
+            
+        }
+    }
+}

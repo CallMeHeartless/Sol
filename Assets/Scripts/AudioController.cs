6997bc455f3406bdc282fb8c2fe8e3594cf03f77
Kerry <kerry.pellett@gmail.com>
2018-10-13 14:26:47 +1300

    Culled some sounds. Implemented audiocontroller

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -6,23 +6,51 @@ public class AudioController : MonoBehaviour {
 
     static AudioController instance;
 
-    public enum ESOUND {
-
-    }
+    private AudioSource[] sounds;
 
 	// Use this for initialization
 	void Start () {
         instance = this;
+        sounds = GetComponents<AudioSource>();
+        foreach(AudioSource sound in sounds) {
+            Debug.Log(sound.clip.name);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    
 	}
 
-    public static void PlaySingleSound(ESOUND sound) {
-        switch (sound) {
-            
+    // Plays a single sound
+    public static void PlaySingleSound(string sound) {
+        AudioSource audio = instance.GetSource(sound);
+        if (audio) {
+            if (!audio.isPlaying) {// Stop looping sounds from starting up again
+                audio.Play();
+            }
+        } else {
+            Debug.Log("Sound not found - null reference.");
         }
+    }
+
+    // Stops a sound (assumed to be looping)
+    public static void StopSingleSound(string sound) {
+        AudioSource audio = instance.GetSource(sound);
+        if (audio) {
+            audio.Play();
+        } else {
+            Debug.Log("Sound not found - null reference.");
+        }
+    }
+
+    private AudioSource GetSource(string sound) {
+        foreach(AudioSource audio in sounds) {
+            if(audio.clip.name == sound) {
+                return audio;
+            }
+        }
+
+        return null;
     }
 }
